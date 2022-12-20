4f3601e3022a0738c324bec6e033a91759af83ab
Fawaz Oduola <oduolafawaz@gmail.com>
2022-12-20 12:03:13 +0100

    done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
-export default function handleResponseFromAPI(promise){
-    let obj = {status: 200, body: "success"}
-    return promise
-        .then(()=>obj)
-        .catch(()=>new Error)
-        .finally(()=>{console.log('Got a response from the API')});
+export default function handleResponseFromAPI (promise) {
+  const obj = { status: 200, body: 'success' }
+  return promise
+    .then(() => obj)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API') })
 }
