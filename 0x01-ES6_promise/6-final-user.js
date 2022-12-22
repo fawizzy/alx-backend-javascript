7946356d01c6c0c60678db6edf57d123215fcdfb
Fawaz Oduola <oduolafawaz@gmail.com>
2022-12-22 01:48:17 +0100

    done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,15 @@ export default function handleProfileSignup (firstName, lastName, fileName) {
   const s = signUpUser(firstName, lastName)
   const u = uploadPhoto(fileName)
 
-  return Promise.allSettled([s, u]).then((val) => val)
+  return Promise.allSettled([s, u]).then((vals) => {
+    const arr = []
+    vals.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        arr.push({ status: val.status, value: val.value })
+      } else {
+        arr.push({ status: val.status, value: val.reason.message })
+      }
+    })
+    return arr
+  })
 }
