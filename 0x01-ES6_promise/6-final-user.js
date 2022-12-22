685d4b05065a6569096d9a60a400fe4ccb109e14
Fawaz Oduola <oduolafawaz@gmail.com>
2022-12-22 01:53:43 +0100

    done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,19 @@
 import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
 
-export default function handleProfileSignup (firstName, lastName, fileName) {
+export default async function handleProfileSignup (firstName, lastName, fileName) {
   const s = signUpUser(firstName, lastName)
   const u = uploadPhoto(fileName)
 
-  return Promise.allSettled([s, u]).then((vals) => {
+  const vals = await Promise.allSettled([s, u])
     const arr = []
     vals.forEach((val) => {
-      if (val.status === 'fulfilled') {
-        arr.push({ status: val.status, value: val.value })
-      } else {
-        arr.push({ status: val.status, value: val.reason.message })
-      }
+        if (val.status === 'fulfilled') {
+            arr.push({ status: val.status, value: val.value })
+        } else {
+            arr.push({ status: val.status, value: `Error: ${val.reason.message}`})
+        }
     })
+    console.log(arr)
     return arr
-  })
 }
